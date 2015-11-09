44fdac077baa288bbc151fb0f8520f1b914ed26c
Samson <bonfante@steptools.com>
2015-11-09 11:44:11 -0500

    Fixed anchors so things scale properly.

--- Ev3DL/Build.Designer.cs
+++ Ev3DL/Build.Designer.cs
@@ -39,55 +39,68 @@
             // 
             // tbBuildOutput
             // 
-            this.tbBuildOutput.Location = new System.Drawing.Point(12, 103);
+            this.tbBuildOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbBuildOutput.Location = new System.Drawing.Point(9, 84);
+            this.tbBuildOutput.Margin = new System.Windows.Forms.Padding(2);
             this.tbBuildOutput.Multiline = true;
             this.tbBuildOutput.Name = "tbBuildOutput";
             this.tbBuildOutput.ReadOnly = true;
             this.tbBuildOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbBuildOutput.Size = new System.Drawing.Size(257, 154);
-            this.tbBuildOutput.TabIndex = 0;
+            this.tbBuildOutput.Size = new System.Drawing.Size(194, 126);
+            this.tbBuildOutput.TabIndex = 4;
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(194, 28);
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(146, 23);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Size = new System.Drawing.Size(56, 19);
+            this.btnBrowse.TabIndex = 2;
             this.btnBrowse.Text = "Browse...";
             this.btnBrowse.UseVisualStyleBackColor = true;
             this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // tbFile
             // 
-            this.tbFile.Location = new System.Drawing.Point(12, 29);
+            this.tbFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFile.Location = new System.Drawing.Point(9, 24);
+            this.tbFile.Margin = new System.Windows.Forms.Padding(2);
             this.tbFile.Name = "tbFile";
-            this.tbFile.Size = new System.Drawing.Size(176, 22);
-            this.tbFile.TabIndex = 2;
+            this.tbFile.Size = new System.Drawing.Size(133, 20);
+            this.tbFile.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 17);
-            this.label1.TabIndex = 3;
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
             this.label1.Text = "File:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Location = new System.Drawing.Point(9, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 17);
-            this.label2.TabIndex = 4;
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 0;
             this.label2.Text = "Build Output:";
             // 
             // btnBuild
             // 
-            this.btnBuild.Location = new System.Drawing.Point(194, 57);
+            this.btnBuild.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuild.Location = new System.Drawing.Point(146, 46);
+            this.btnBuild.Margin = new System.Windows.Forms.Padding(2);
             this.btnBuild.Name = "btnBuild";
-            this.btnBuild.Size = new System.Drawing.Size(75, 23);
-            this.btnBuild.TabIndex = 5;
+            this.btnBuild.Size = new System.Drawing.Size(56, 19);
+            this.btnBuild.TabIndex = 3;
             this.btnBuild.Text = "Build";
             this.btnBuild.UseVisualStyleBackColor = true;
             this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
@@ -98,15 +111,16 @@
             // 
             // BuildFrm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 263);
+            this.ClientSize = new System.Drawing.Size(212, 214);
             this.Controls.Add(this.btnBuild);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbFile);
             this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.tbBuildOutput);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "BuildFrm";
             this.ShowIcon = false;
             this.Text = "Build";
